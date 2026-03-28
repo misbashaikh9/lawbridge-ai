91db86e53c09737e6d458836979886d09c47e4c5
Misba Shaikh <misbamshaikh7@gmail.com>
2026-03-28 23:39:38 +0530

    connect frontend with deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lawbridge-ai-1.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
